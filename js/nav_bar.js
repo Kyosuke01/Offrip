330b0af9ae9831a9b9a99df5ad1529b7b59b960a
Kyosuke <fire.gaming95800@gmail.com>
2023-12-23 18:15:46 +0100

    functionnality of click outside was added for the nav bar

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -5,48 +5,72 @@ $.easing.easeInOutQuad = function (x, t, b, c, d) {
 
 $(document).ready(function () {
     var isNavVisible = false;
-    var isAnimating = false; // Ajout de la variable de verrouillage
+    var isAnimating = false;
 
     $("#nav_burger").click(function () {
-        // Vérifier si l'animation est en cours
         if (isAnimating) {
             return;
         }
 
         var $navLinks = $(".nav_link");
-        var $this = $(this); // Stocker la référence à $(this)
+        var $this = $(this);
 
         if (!isNavVisible) {
-            // Si la barre de navigation n'est pas visible, l'animer vers le haut
-            isAnimating = true; // Verrouiller l'animation
+            isAnimating = true;
             $.when(
                 $navLinks.css("display", "flex").animate({ marginTop: "0" }, {
                     duration: 500,
                     easing: "easeInOutQuad"
                 }),
-                $this.toggleClass("active") // Ajouter ou supprimer la classe "active"
+                $this.toggleClass("active")
             ).done(function () {
-                isAnimating = false; // Déverrouiller l'animation après la fin
+                isAnimating = false;
             });
         } else {
-            // Si la barre de navigation est visible, l'animer vers le bas
-            isAnimating = true; // Verrouiller l'animation
+            isAnimating = true;
             $.when(
                 $navLinks.animate({ marginTop: "-10vh" }, {
                     duration: 500,
                     easing: "easeInOutQuad"
                 }),
-                $this.toggleClass("active") // Ajouter ou supprimer la classe "active"
+                $this.toggleClass("active")
             ).done(function () {
                 $navLinks.css("display", "none");
-                isAnimating = false; // Déverrouiller l'animation après la fin
+                isAnimating = false;
             });
         }
 
-        // Inverser l'état de visibilité
         isNavVisible = !isNavVisible;
     });
+
+    // ADDED
+
+    // Gestionnaire d'événements pour le clic sur l'ensemble du document
+    $(document).click(function (event) {
+        var $navContainer = $("header");
+        var $navBurger = $("#nav_burger");
+
+        // Vérifier si la cible du clic n'est pas dans la barre de navigation
+        // ET que la barre de navigation est visible
+        // ET que la cible du clic n'est pas un élément de la barre de navigation
+        if (
+            !$(event.target).closest($navContainer).length &&
+            isNavVisible &&
+            !$(event.target).closest($navBurger).length
+        ) {
+            $navBurger.click(); // Simuler un clic sur #nav_burger
+        }
+    });
+
+    // Gestionnaire d'événements pour le clic sur la barre de navigation
+    $("header").click(function (event) {
+        // Empêcher la propagation du clic si la barre de navigation est visible
+        if (isNavVisible) {
+            event.stopPropagation();
+        }
+    });
 });
+
 
 $(document).ready(function () {
     $(".nav_link a").mouseover(function () {
